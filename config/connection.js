cb560037c84d3b4bb36ff471c293dc20710bcfcd
Kyle McWilliams <kmac52694@gmail.com>
2021-10-16 16:43:27 -0500

    changing to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
-const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/regex-tutorial',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+let sequelize;
+
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
   }
-);
-module.exports = mongoose.connection;
+
+module.exports = sequelize;
