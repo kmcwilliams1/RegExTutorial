7a3e093327050d77c12ea8eb6823fec1e0b9638d
Kyle McWilliams <kmac52694@gmail.com>
2021-10-16 16:41:37 -0500

    changing to mongoose

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,12 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const mongoose = require('mongoose');
 
-let sequelize;
-
-if(process.env.JAWSDB_URL){
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-      }
-    );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/regex-tutorial',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
   }
-
-module.exports = sequelize;
+);
+module.exports = mongoose.connection;
